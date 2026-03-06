66e09b6d792d85f0a614d59f68cc2924c9bfbe1f
Max <102370987+m-a-x-w@users.noreply.github.com>
2026-03-05 21:24:57 -0500

    fix /thoughts (v2)

--- src/routes/thoughts/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import { getPosts } from '$lib/posts';
-import type { PageLoad } from './$types';
-
-export const load: PageLoad = async () => {
-	const posts = await getPosts();
-	return { posts };
-};
